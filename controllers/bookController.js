817125aa3f241f3d8372f5f0df3709c6fc61e168
unknown <jcontreras39@gmail.com>
2020-07-16 20:48:33 -0500

    css edit10

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the bookController
+// Defining methods for the bookController this will be helpful
 module.exports = {
   findAll: function(req, res) {
     db.Book.find(req.query)
